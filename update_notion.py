a2f1a6ee4970418680efbd5c2b646edad83bf9c5
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-08-16 06:29:41 +1200

    Create update_notion.py

---
+++ update_notion.py
@@ -0,0 +1,30 @@
+import gspread
+from notion.client import NotionClient
+import os
+
+# Ler as variáveis de ambiente
+google_sheet_credentials_json = os.getenv('GOOGLE_SHEET_CREDENTIALS_JSON')
+notion_token = os.getenv('NOTION_TOKEN')
+
+# Configurar cliente do Google Sheets e Notion
+gc = gspread.service_account_from_dict(google_sheet_credentials_json)
+client = NotionClient(token_v2=notion_token)
+
+# ID da planilha do Google Sheets
+spreadsheet_id = '1rKgwERiE6CQhK69sBun9N2dsGegAHTWctPS_KiHVM_8'
+worksheet_name = 'QUOTES'  # Nome da planilha
+sh = gc.open_by_key(spreadsheet_id)
+worksheet = sh.worksheet(worksheet_name)
+
+# Percorrer todas as linhas da planilha
+data = worksheet.get_all_records()
+for row in data:
+    notion_page_url = row['URL']
+    new_value = row['PRICE']
+
+    # Pegar a página Notion correspondente
+    notion_page = client.get_block(notion_page_url)
+
+    # Atualizar o valor na propriedade "Price" da página no Notion
+    notion_property = notion_page.collection.get_schema_property('Price')
+    notion_page.set_property(notion_property['id'], new_value)
