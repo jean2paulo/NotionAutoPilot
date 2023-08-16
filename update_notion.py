51e4dc64fdede36134ee8e3332cb4c1379366931
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-08-17 05:36:26 +1200

    Update update_notion.py

--- update_notion.py
+++ update_notion.py
@@ -41,5 +41,5 @@ for row in data:
     # Atualizar o valor na propriedade "Price" da página no Notion
     notion.pages.update(
         formatted_page_id,
-        properties={"Price":{"rich_text":[{"type": "number","number": price}]}},
+        properties={"Price":[{"type": "number","number": price}]},
     )
