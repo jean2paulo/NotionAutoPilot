e6f6274665e27e80f8ef9a9eaf4e36b3aa191c88
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-08-18 20:15:45 +1200

    update webpage

--- extract_fii_data.py
+++ extract_fii_data.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL da página da qual você deseja recuperar o item
-url = "https://fiis.com.br/vino11"
+url = "https://www.fundsexplorer.com.br/funds/vino11"
 
 # Enviando uma solicitação GET para obter o conteúdo da página
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
