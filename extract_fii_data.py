78219dc4c31e6d49c238abc8d1dba5708e3c8777
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-08-18 19:56:33 +1200

    debug html page

--- extract_fii_data.py
+++ extract_fii_data.py
@@ -11,16 +11,18 @@ response = requests.get(url)
 if response.status_code == 200:
     # Analisando o conteúdo da página com o Beautiful Soup
     soup = BeautifulSoup(response.content, "html.parser")
+
+    print(soup.prettify())
     
     # Encontrando o elemento que contém o item desejado (substitua "SELETOR_CSS" pelo seletor correto)
-    item_element = soup.select_one("SELETOR_CSS")
+    #item_element = soup.select_one("SELETOR_CSS")
     
     # Verificando se o elemento foi encontrado
-    if item_element:
+    #if item_element:
         # Recuperando o texto do item
-        item_text = item_element.get_text()
-        print("Item recuperado:", item_text)
-    else:
-        print("Elemento não encontrado.")
+        #item_text = item_element.get_text()
+        #print("Item recuperado:", item_text)
+    #else:
+        #print("Elemento não encontrado.")
 else:
     print("Falha na solicitação. Código de status:", response.status_code)
