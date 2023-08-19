1d3908bae45866b36a9ffa7c9d18e90767745c61
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-08-19 15:45:32 +1200

    Update extract_fii_data.py

--- extract_fii_data.py
+++ extract_fii_data.py
@@ -5,9 +5,8 @@ from bs4 import BeautifulSoup
 url = "https://www.fundsexplorer.com.br/funds/vino11"
 
 # Enviando uma solicitação GET para obter o conteúdo da página
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-#response = requests.get(url, headers=headers)
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get(url, headers=headers)
 
 # Verificando se a solicitação foi bem-sucedida
 if response.status_code == 200:
