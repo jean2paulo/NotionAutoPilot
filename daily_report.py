c495ba2cfaa1abdde3ca5f6e261cb22532297f28
Jean Paulo <jean2paulo@gmail.com>
2023-10-14 12:28:41 -0300

    update daily report flow

--- daily_report.py
+++ daily_report.py
@@ -26,12 +26,11 @@ commitment_message = commitment.check_today_commitment(notion)
 tasks_message = read_tasks.check_all_tasks(notion)
 financial_message = financial_month.check_financial_month_totals(notion)
 
-full_message = "Bom dia Jean! ☀️ \nEsse eh o seu reporte para o dia de hoje:\n\n"
+full_message = "Bom dia Jean! \nEsse eh o seu reporte para o dia de hoje:\n\n"
 full_message += f"{financial_message}\n\n"
 full_message += f"{birthday_message}\n\n"
 full_message += f"{calendar_message}\n\n"
 full_message += f"{commitment_message}\n\n"
 full_message += f"{tasks_message}\n"
 
-with open("daily_report_output.txt", "w") as file:
-    file.write(full_message)
+print(full_message)
