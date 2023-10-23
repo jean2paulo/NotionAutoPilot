1d57b349e7faeb5957dbf25320a169cd1f89b886
Jean Paulo de Almeida <jean2paulo@gmail.com>
2023-10-23 07:06:40 -0300

    Update financial_month.py

--- notion/financial_month.py
+++ notion/financial_month.py
@@ -1,7 +1,7 @@
 from notion import utils as notion_utils
 
 # Projects Notion Page
-FINANCIAL_MONTH_PAGE_ID = "ecc237c39382476e8cefc7224ca5f87c"
+FINANCIAL_MONTH_PAGE_ID = "4a7dd04804cd42b78c84f38edd5924fa"
 
 def check_financial_month_totals(notion):
     data = notion_utils.notion_query(notion, FINANCIAL_MONTH_PAGE_ID, 
